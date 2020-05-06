aa8cb501ba5067c66ae5e92be766157f9cc93373
jbqneto <jbqneto@gmail.com>
2020-05-06 00:13:32 -0300

    pill style and form to get files created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container fluid>
+    <v-form>
+      <v-file-input multiple chips v-model="files"></v-file-input>
+    </v-form>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" :amount="word.amount" :name="word.name"></Pill>
+    </div>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Pill from "../components/Pill";
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld
+    Pill
+  },
+  data: function() {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "you", amount: 900 },
+        { name: "he", amount: 200 },
+        { name: "she", amount: 300 },
+        { name: "that", amount: 200 },
+        { name: "try", amount: 180 },
+        { name: "not", amount: 120 },
+        { name: "all", amount: 321 },
+        { name: "yes", amount: 81 },
+        { name: "why", amount: 16 }
+      ]
+    };
   }
 };
 </script>
+
+<style scoped>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
